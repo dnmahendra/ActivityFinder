caf423cd178d0ec012b884d47b07ff632305ed1f
Mahendra <1987mahendrad@gmail.com>
2016-04-08 15:08:07 +0530

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-15.times do |n|
-    title =  Faker::Team.sport 
-    description =  Faker::Hipster.paragraph 
+10.times do |n|
+    name = Faker::Internet.user_name
+    email = Faker::Internet.email
+    password = '12345'
+    gender = 'male'
+    age = Faker::Number.between(10, 60)
+    avatar = Faker::Avatar.image("100x100")
     location = Faker::Address.city
-    begins = Faker::Date.forward(30)
-    user_id  = 2
-    Activity.create!(title: title, 
-                 description: description,
-                 location: location, 
-                 begins: begins,
-                 user_id: user_id)
-   end 
+    User.create!(name: name,
+                email: email,
+                password_digest: password,
+                gender: gender,
+                age: age,
+                avatar: avatar,
+                location: location)
+end
+
+# 15.times do |n|
+#     title =  Faker::Team.sport 
+#     description =  Faker::Hipster.paragraph 
+#     location = Faker::Address.city
+#     begins = Faker::Date.forward(30)
+#     user_id  = Faker::Number.between(1, 10)
+#     Activity.create!(title: title, 
+#                  description: description,
+#                  location: location, 
+#                  begins: begins,
+#                  user_id: user_id)
+#    end 
+
+
